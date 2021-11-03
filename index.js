e00da3ce74e156dacc4026f2e3f7333080ef6990
Zakorchemnyi <93659659+Zakorchemnyi@users.noreply.github.com>
2021-11-03 17:56:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,37 @@ console.log(data.City);
 let Cars={};
 Cars["Volksvagen and Audi"]=true;
 console.log(Cars);
-console.log(Cars);
+let z=i-=a;
+console.log(z);
+console.log(i**i);
+console.log(a/i);
+if (i!=a)
+{console.log('"i ne dorivnue a"')};
+console.log(i===a);
+console.log(pens==pencils);
+let x=18;
+let result=x===18
+? console.log("'x virno'")
+: console.log("'x nevirno'");
+if (x>i || x>=a){console.log("'X ye bilshe'")}
+else {console.log("'X NE ye bilshe'")};
+const day="2";
+switch (day){
+case "1": {console.log("Monday");
+} break;
+case "2": {console.log("Tuesday");
+} break;
+case "3": {console.log("Wednesday");
+} break;
+case "4": {console.log("Thursday");
+} break;
+case "5": {console.log("Friday");
+} break;
+case "6": {console.log("Saturday");
+} break;
+case "7": {console.log("Sunday");
+} break;
+default: {
+    console.log("Incorrect");
+}
+}
